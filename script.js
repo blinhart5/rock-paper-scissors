2021a33759ebd5f241345aff3c12d18172199df6
blinhart5 <blinhart5@gmail.com>
2022-01-22 11:30:39 -0600

    Started script from scratch

--- script.js
+++ script.js
@@ -1,63 +1,44 @@
-
-// computer player randomly returns 'rock', 'paper' or 'scissors'.; 
-function computerPlay() {
-    let number = Math.floor(Math.random() * 3);
-    if (number == 0) {
-        return "rock";
-    } else if (number == 1) {
-        return "paper";
-    } else {
-        return "scissors";
-            }
-    };
-   
-
-let playerScore = 0;
-let compScore = 0;
-
-function playRound(playerSelection, computerSelection) {
-            // player winning rounds. 
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-            playerScore++;
-            return  "Rock Beats Scissors, You Win!";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-            playerScore++;
-            return "Paper Covers Rock, You Win!";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-            playerScore++;
-            return "Scissor Cuts Paper, You Win!";
-
-            // computer winning rounds.
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-            compScore++;
-            return "Rock Beats Scissors, You Lose!";
-            
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-            compScore++;
-            return "Paper covers rock, You Lose!";
-            
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-            compScore++;
-            return "Scissor Cuts Paper, You Lose!";
-            
-    } else {
-            // when it's a tie. 
-        return "It's A Tie!"
-    }
-}
-
-
-
-
-const playerInput = window.prompt("Rock, Paper, or Scissors?");
-   
-const playerSelection = playerInput.toLowerCase();
 const computerSelection = computerPlay(); 
+const playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+let tieScore = 0;
+let playerScore = 0;
+let compScore = 0; 
 
+function computerPlay() {
+        const number = Math.floor(Math.random() *3); 
+        if (number === 0) {
+                return 'rock';
+        } else if (number === 1) { 
+                return 'paper';
+        } else {
+                return 'scissors';
+        }
+}
+ 
+ function playRound(playerSelection, computerSelection) {
+                        if (playerSelection === 'rock' && computerSelection === 'paper') {
+                                 compScore++;
+                                 return 'Paper Covers Rock. You Lose.'; 
+                        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+                                playerScore++;
+                                return 'Rock crushes Scissors. You Win';
+                        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+                                compScore++;
+                                return 'Scissors cuts paper. You Lose.';
+                        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+                                playerScore++;
+                                return 'Paper Covers Rock. You win.'; 
+                        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+                                compScore++;
+                                return 'Rock crushes scissors. You Lose';
+                        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+                                playerScore++;
+                                return 'Scissors cuts paper. You win!';
+                        } else { 
+                                tieScore++;
+                                return "It's a tie!";
+                        }
+                        
+                }
 console.log(playRound(playerSelection, computerSelection));
-console.log(playerScore, compScore);
-console.log(playerSelection,"/", computerSelection);
-
-
-
 
