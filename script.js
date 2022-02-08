a13f823689587192996ba22d3d1ceadb5c8cd793
blinhart5 <blinhart5@gmail.com>
2022-02-07 19:16:24 -0600

    Added comments and corrected spelling errors

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 const buttons = document.querySelectorAll('button');
+
 let tieScore = 0;
 let playerScore = 0;
 let compScore = 0; 
@@ -8,11 +9,13 @@ const rock = document.getElementById('#rock');
 const paper = document.getElementById('#paper');
 const scissors = document.getElementById('#scissors'); 
 
-
-
 const results = document.getElementById('results');
 const pScore = document.getElementById('pScore');
 const cScore = document.getElementById('cScore');
+
+cScore.innerText = `Computer Score: ${compScore}`;
+pScore.innerText = `Player Score: ${playerScore}`;
+results.innerText = '';
 
 const playAgainContainer = document.createElement('div');
 playAgainContainer.classList.add('playAgainContainer');
@@ -22,57 +25,40 @@ const playAgainBtn = document.createElement('button');
 playAgainBtn.classList.add('playAgainBtn');
 playAgainBtn.innerText = 'Play Again!';
 
-
-
-
+/* Play again button refreshes page, restarts game.  */
 playAgainBtn.addEventListener('click', refreshPage);
-
 function refreshPage() {
-        window.parent.location = window.parent.location;
-        
+        window.parent.location = window.parent.location;        
 }
 
-cScore.innerText = `Computer Score: ${compScore}`;
-pScore.innerText = `Player Score: ${playerScore}`;
-results.innerText = 'Who`s it gonna be?';
-
-
-
-
+/* Get player selection start gameplay.  */
 buttons.forEach((button) => {
         button.addEventListener('click', (getSelection) => {
                 let playerSelection = getSelection.target.id;
                 let computerSelection = computerPlay();
-                console.log(playRound(playerSelection, computerSelection));
+                (playRound(playerSelection, computerSelection));
                 isGameOver(playerScore, compScore);
-                
-               
-
-                
-console.log(playerSelection);
-console.log(computerSelection); 
         })
         
 })
 
+/* Disable buttons when game is over */
 function disableBtns(){
         buttons.forEach((button) => {
-                button.setAttribute('disabled' , true); 
-                
+                button.setAttribute('disabled' , true);         
         })
 }
 
-
-
+/* Check for game over. If Game is over: 
+disable choice buttons, reveal 'Play Again' button. */
 function isGameOver(playerScore, compScore){
         if (playerScore === 5 || compScore === 5) {
                playAgainContainer.append(playAgainBtn);
                 disableBtns(); 
                 if (playerScore > compScore) {
-                        results.innerText = 'GAME OVER! HUMAN WON!';
-                        
+                        results.innerText = 'GAME OVER! YOU WON!';
                 } else if (compScore > playerScore) {
-                        results.innerText = 'GAME OVER! THE COMPUTERS ARE TAKING OVER THE WORLD!';
+                        results.innerText = 'GAME OVER! THE COMPUTERS ARE TAKING OVER!!';
                 } else {
                         results.innerText = `It's a draw!`;
                 }
@@ -80,12 +66,7 @@ function isGameOver(playerScore, compScore){
 
 }
 
-let player = 'player'; 
-let computer = 'computer'; 
-
-    
- 
-
+/* Get Computer Selection */
 function computerPlay() {
         const number = Math.floor(Math.random() * 3); 
         if (number === 0) {
@@ -98,9 +79,10 @@ function computerPlay() {
 }
 
 
-
+/* Compare player choice and computer choice. Updates Scores. Checks to see 
+if game is over.  */
 function playRound(playerSelection, computerSelection) {
-        
+
                         if (playerSelection === 'rock' && computerSelection === 'paper') {
                                 compScore++; 
                                 cScore.innerText = `Computer Score: ${compScore}`;
@@ -111,34 +93,32 @@ function playRound(playerSelection, computerSelection) {
                                 playerScore++;
                                 pScore.innerText = `Player Score: ${playerScore}`;
                                 isGameOver(playerScore, compScore);
-                                return results.innerText = 'Rock crushes Scissors. You Win';
+                                return results.innerText = 'Rock Crushes Scissors. You Win';
                         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
                                 compScore++;
                                 cScore.innerText = `Computer Score: ${compScore}`;
                                  isGameOver(playerScore, compScore);
-                                return results.innerText = 'Scissors cuts paper. You Lose.';
+                                return results.innerText = 'Scissors Cuts Paper. You Lose.';
                         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
                                 playerScore++;
-                                pScore.innerText = `Player Score: ${playerScore};`
+                                pScore.innerText = `Player Score: ${playerScore}`;
                                  isGameOver(playerScore, compScore);
-                                return results.innerText = 'Paper Covers Rock. You win.'; 
+                                return results.innerText = 'Paper Covers Rock. You Win.'; 
                         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
                                 compScore++;
                                 cScore.innerText = `Computer Score: ${compScore}`;
                                isGameOver(playerScore, compScore);
-                                return results.innerText = 'Rock crushes scissors. You Lose';
+                                return results.innerText = 'Rock Crushes Scissors. You Lose';
                         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
                                 playerScore++;
                                 pScore.innerText = `Player Score: ${playerScore}`;
                                 isGameOver(playerScore, compScore);
-                                return 'Scissors cuts paper. You win!';
+                                return 'Scissors Cuts Paper. You Win!';
                         } else { 
                                 tieScore++;
                                isGameOver(playerScore, compScore);
-                                return results.innerText = "It's a tie!";
-                        }
-                 
-              
+                                return results.innerText = "It's A Tie!";
+                        }              
 }
 
 
